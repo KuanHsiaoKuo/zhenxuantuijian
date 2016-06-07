bb0771de56d55fb7d7088a087eb10b62985a9fcc
hughohoho <johnson_hugh@163.com>
2016-06-07 22:12:06 +0800

    配置静态文件路径

--- django_env/zhenxuantuijian/zhenxuantuijian/settings.py
+++ django_env/zhenxuantuijian/zhenxuantuijian/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
 
 #自定义用户model
 AUTH_USER_MODEL = 'zxtj_site.User'
