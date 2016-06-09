b68ebfbe961c687c514ea3969baec785a7575a4d
hughohoho <johnson_hugh@163.com>
2016-06-09 12:56:23 +0800

    baicaio爬虫-scrapy shell交互调试之后在爬虫代码中获取想要的中文信息

--- spiders/baicaio/baicaio/spiders/tutorial_spider.py
+++ spiders/baicaio/baicaio/spiders/tutorial_spider.py
@@ -11,13 +11,14 @@ class Tutorial_Spider(scrapy.spiders.Spider):
     # 因此，第一个被获取到的页面将是其中之一。
     # 后续的URL则从初始的URL获取到的数据中提取。   
     start_urls = [
-        "http://www.baicaio.com",
+        "http://www.baicaio.com/2016/06/09/180093.html",
     ]
     # 是spider的一个方法。 被调用时，每个初始URL完成下载后生成的 Response
     # 对象将会作为唯一的参数传递给该函数。 该方法负责解析返回的数据(response
     # data)，提取数据(生成item)以及生成需要进一步处理的URL的 Request 对象。
     def parse(self,response):
-        filename = response.url.split("/")[-2]
-        pdb.set_trace()
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        title = response.xpath(
+            '//*[@id="content"]/ul/li/h1/a/@title'
+            ).extract()
+        print title  # 打印的是列表，不显示中文
+        print title[0] #直接打印中文
