bd705edbb5aefaa0ba03fb0d17de5367246c1219
hughohoho <johnson_hugh@163.com>
2016-06-09 11:40:00 +0800

    baicaio爬虫-完成教程爬虫的爬取逻辑

---
+++ spiders/baicaio/baicaio/spiders/tutorial_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class Tutorial_Spider(scrapy.spiders.Spider):
+    # 用于区别Spider。
+    # 该名字必须是唯一的，您不可以为不同的Spider设定相同的名字。
+    name = "baicaio_tutorial"
+    allowed_domains = ['baicaio.com']
+    # 包含了Spider在启动时进行爬取的url列表。
+    # 因此，第一个被获取到的页面将是其中之一。
+    # 后续的URL则从初始的URL获取到的数据中提取。   
+    start_urls = [
+        "http://www.baicaio.com",
+    ]
+    # 是spider的一个方法。 被调用时，每个初始URL完成下载后生成的 Response
+    # 对象将会作为唯一的参数传递给该函数。 该方法负责解析返回的数据(response
+    # data)，提取数据(生成item)以及生成需要进一步处理的URL的 Request 对象。
+    def parse(self,response):
+        filename = response.url.split("/")[-2]
+        with open(filename, 'wb') as f:
+            f.write(response.body)
