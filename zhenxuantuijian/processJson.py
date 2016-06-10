e973b08f10448548f20bd6ecd2aa50d20b621111
hughohoho <johnson_hugh@163.com>
2016-06-10 12:49:19 +0800

    baicaio爬虫-简单学习json文件的读取,可以通过字典取出相应的值

--- zhenxuantuijian/processJson.py
+++ zhenxuantuijian/processJson.py
@@ -1,10 +1,17 @@
 import json
 import os
-# from blog.models import Article
+
+
+# 用Python获得一些有关系统的各种信息
+# 默认设置了DJANGO_SETTINGS_MODULE，所以任何一个django project中的*.py文件都能够正常的使用项目中的数据模型操作。
+# 所以这个要在导入模型之前声明
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zhenxuantuijian.settings")
+
+
+from blog.models import Article
 fin = open('baicaio_items.json', 'r')
 s = json.load(fin)
 print (type(s)) # 最外面是数组，对应python的list
 for line in s:
     print (type(line)) # 里面每条记录其实就是字典
-    print (line)
+    print (line['title'])
